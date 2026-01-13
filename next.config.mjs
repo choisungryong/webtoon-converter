f5037ec84d83dd09598f83e8897ed29ef973d957
Choi Sung Ryong <twinspa0713@gmail.com>
2026-01-13 20:59:18 +0900

    fix: configuring next.config.mjs to use local setupDevPlatform

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig = {
     // Cloudflare Pages 환경을 위해 최적화
     reactStrictMode: true,
 };
+
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+}
 
 export default nextConfig;
