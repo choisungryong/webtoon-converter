5ba4539ce9961684491eb79ffc1ee7ac1cbc01b4
Choi Sung Ryong <twinspa0713@gmail.com>
2026-01-20 12:48:13 +0900

    fix : GA 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,19 @@ export default function RootLayout({
                     strategy="afterInteractive"
                     crossOrigin="anonymous"
                 />
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-SWE71HTWN1"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-SWE71HTWN1');
+                    `}
+                </Script>
                 <AntdRegistry>
                     <main className="flex-1">
                         {children}
