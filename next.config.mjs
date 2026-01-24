fe41919611639a2062f6d02a3fe8455e0955ddd5
Choi Sung Ryong <twinspa0713@gmail.com>
2026-01-24 19:58:10 +0900

    fix : kakao share fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,20 @@ if (process.env.NODE_ENV === 'development') {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'banatoon.app',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
