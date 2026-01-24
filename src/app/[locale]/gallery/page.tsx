9c2a23beee23745fc4f12af473850519fed38431
Choi Sung Ryong <twinspa0713@gmail.com>
2026-01-25 02:17:16 +0900

    language pack accept

--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import { useEffect, useState, useRef, Suspense } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { Spin, Modal, message } from 'antd';
